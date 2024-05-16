ad7103be331510518761463e7ca600f28f5e9fd0
Wolf <wolf@tgwolf.com>
2024-05-16 02:09:40 +0100

    min-version: 3.9 for python

--- setup.py
+++ setup.py
@@ -30,6 +30,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.8',
+    python_requires='>=3.9',
     install_requires=required,
 )
