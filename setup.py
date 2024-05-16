7d6e8ad7c02cb22f3a9a451f102e1cdf5a0b4358
Wolf <wolf@tgwolf.com>
2024-05-16 01:51:46 +0100

    add 'packaging' to requirements

--- setup.py
+++ setup.py
@@ -2,7 +2,29 @@
 
 """Setup script."""
 
+import pip
+
 from setuptools import setup, find_packages
+
+links = []
+requires = []
+
+try:
+    requirements = pip.req.parse_requirements('requirements.txt')
+except:
+    # new versions of pip requires a session
+    requirements = pip.req.parse_requirements(
+        'requirements.txt', session=pip.download.PipSession())
+
+for item in requirements:
+    # we want to handle package names and also repo urls
+    if getattr(item, 'url', None):  # older pip has url
+        links.append(str(item.url))
+    if getattr(item, 'link', None): # newer pip has link
+        links.append(str(item.link))
+    if item.req:
+        requires.append(str(item.req))
+
 
 with open("README.md", 'r', encoding='UTF-8') as f:
     long_description: str = f.read()
